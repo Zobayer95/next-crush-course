7520257eae9d5137709623317effb3bfda347515
Zobayer Abdullah <82441075+Zobayer95@users.noreply.github.com>
2024-07-26 21:23:38 +0600

    Update page.js

--- next14-crash-course-main/next-crash-course-main/app/page.js
+++ next14-crash-course-main/next-crash-course-main/app/page.js
@@ -2,10 +2,7 @@ export default function Home() {
     return (
         <main className="mt-10">
             <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                animi placeat cupiditate sapiente iure aliquam consequuntur, non
-                blanditiis facilis voluptatibus, dolorum itaque! Rem eius
-                repellat, dolores in temporibus aliquam alias.
+                The child class will inherit all the public and protected properties and methods from the parent class. In addition, it can have its own properties and methods.
             </div>
         </main>
     );
