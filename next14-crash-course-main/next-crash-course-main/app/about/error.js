7f1f8ebb5dc5f1bae7e4855033f634312aafa463
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2026-06-08 23:04:22 +0600

    Update error.js

--- next14-crash-course-main/next-crash-course-main/app/about/error.js
+++ next14-crash-course-main/next-crash-course-main/app/about/error.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function Error({ error, reset }) { 
     useEffect(() => {       
-        // Log the error to an error reporting service
+        // Log the error to an error reporting service 
         console.error(error);  
     }, [error]);      
 
